1822f96e6bcb864e65db2c20282e8d8e198746bd
birchb@localhost <birchb@localhost>
2007-04-15 23:44:45 +1000

    Can do conditionals in main.

--- org/lispin/jlispin/interp/Interpreter.java
+++ org/lispin/jlispin/interp/Interpreter.java
@@ -5,6 +5,7 @@ import org.lispin.jlispin.core.InStream;
 import org.lispin.jlispin.core.Lsymbol;
 import org.lispin.jlispin.core.Parser;
 import org.lispin.jlispin.core.SymbolTable;
+import org.lispin.jlispin.interp.builtin.ConditionalFunction;
 import org.lispin.jlispin.interp.builtin.ConsFunction;
 import org.lispin.jlispin.interp.builtin.DefineFunction;
 import org.lispin.jlispin.interp.builtin.QuoteFunction;
@@ -22,7 +23,9 @@ public class Interpreter {
 		_globalEnvironment.defineVariable(new Lsymbol("quote"), new LazyProcedure(_globalEnvironment, null, new QuoteFunction()));
 		_globalEnvironment.defineVariable(new Lsymbol("set"), new EagerProcedure(_globalEnvironment, null, new SetFunction()));
 		_globalEnvironment.defineVariable(new Lsymbol("define"), new EagerProcedure(_globalEnvironment, null, new DefineFunction()));
+		_globalEnvironment.defineVariable(new Lsymbol("cond"), new LazyProcedure(_globalEnvironment, null, new ConditionalFunction()));
 		_globalEnvironment.defineVariable(SymbolTable.NIL, SymbolTable.NIL);
+		_globalEnvironment.defineVariable(SymbolTable.T, SymbolTable.T);
 		_globalEnvironment.defineVariable(SymbolTable.EOF, SymbolTable.EOF);
 
 	}
