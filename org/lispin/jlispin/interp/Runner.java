f9e3723086a34f055c6bf8be958e363771b945bc
birchb@w0019b91114a1.site <birchb@w0019b91114a1.site>
2007-07-06 17:34:46 +1000

    Added BSF

---
+++ org/lispin/jlispin/interp/Runner.java
@@ -0,0 +1,37 @@
+package org.lispin.jlispin.interp;
+
+import java.io.IOException;
+import java.io.Writer;
+import org.lispin.jlispin.core.Exp;
+import org.lispin.jlispin.core.SymbolTable;
+import org.lispin.jlispin.format.IndentedFormatter;
+import org.lispin.jlispin.io.ConvertEofInStream;
+import org.lispin.jlispin.io.InStream;
+import org.lispin.jlispin.io.IndentStream;
+import org.lispin.jlispin.io.Parser;
+import org.lispin.jlispin.io.StringInStream;
+import org.lispin.jlispin.io.UngettableInStream;
+
+public class Runner {
+
+    public static Exp executeScript(Interpreter interp, StringInStream stream, Writer output) throws LispinException {
+        InStream input = new UngettableInStream(new ConvertEofInStream(new IndentStream(new UngettableInStream(stream),
+                false)));
+        Parser parser = interp.newParser(input);
+        IndentedFormatter formatter = new IndentedFormatter(output, 3);
+        Exp expression = null;
+        Exp result = null;
+        do {
+            expression = parser.read();
+            if (expression.equals(SymbolTable.EOF)) {
+                break;
+            }
+            result = interp.evalInGlobalEnvironment(expression);
+            result.acceptVisitor(formatter);
+            try {
+                output.flush();
+            } catch (IOException ignore) {}
+        } while (true);
+        return result;
+    }
+}
