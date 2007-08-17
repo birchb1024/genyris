da8875d729f7471e0980232fb9b89332a56cf0bd
birchb@w0019b91114a1.site <birchb@w0019b91114a1.site>
2007-08-17 16:40:52 +1000

    Fixed broken unit tests - class name fix.

--- src/org/lispin/jlispin/test/interp/ClassTaggingTests.java
+++ src/org/lispin/jlispin/test/interp/ClassTaggingTests.java
@@ -17,22 +17,22 @@ public class ClassTaggingTests extends TestCase {
 	}
 	
     public void testDefineClass() throws Exception {
-        excerciseEval("(class C)", "(dict (_classes : ((dict (_classname : (StandardClass))))) (_classname : C))");
+        excerciseEval("(class C)", "(dict (_classes : ((dict (_classname : StandardClass)))) (_classname : C))");
         excerciseEval("(class C1 (C))"
-                , "(dict (_superclasses : ((dict (_classes : ((dict (_classname : (StandardClass))))) (_classname : C)))) (_classes : ((dict (_classname : (StandardClass))))) (_classname : C1))");
+                , "(dict (_superclasses : ((dict (_classes : ((dict (_classname : StandardClass)))) (_classname : C)))) (_classes : ((dict (_classname : StandardClass)))) (_classname : C1))");
         }
 
     public void testTagWithColon() throws Exception {
-		excerciseEval("(class Miles)", "(dict (_classes : ((dict (_classname : (StandardClass))))) (_classname : Miles))");
+		excerciseEval("(class Miles)", "(dict (_classes : ((dict (_classname : StandardClass)))) (_classname : Miles))");
 		excerciseEval("(define x 45)", "45");		
         excerciseEval("(x:Miles)", "45");     
-        excerciseEval("(x _classes)", "((dict (_classname : (Bignum))) (dict (_classes : ((dict (_classname : (StandardClass))))) (_classname : Miles)))");     
+        excerciseEval("(x _classes)", "((dict (_classes : ((dict (_classname : StandardClass)))) (_classname : Miles)) (dict (_classname : Bignum)))");     
        	}
 
     public void testTagWithTag() throws Exception {
         excerciseEval("(define Miles (dict(_classname: 'Miles)))", "(dict (_classname : Miles))");
         excerciseEval("(define x 45)", "45");       
         excerciseEval("(tag x Miles)", "45");     
-        excerciseEval("(x _classes)", "((dict (_classname : Miles)) (dict (_classname : (Bignum))))");     
+        excerciseEval("(x _classes)", "((dict (_classname : Miles)) (dict (_classname : Bignum)))");     
         }
 }
