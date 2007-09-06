48c09afb6adaed26811806afc95a3509c22394b0
birchb@w0019b91114a1.site <birchb@w0019b91114a1.site>
2007-09-06 14:01:49 +1000

    Added constants.

---
+++ src/org/lispin/jlispin/core/Constants.java
@@ -0,0 +1,36 @@
+package org.lispin.jlispin.core;
+
+public class Constants {
+	
+	public static final String DYNAMICSCOPECHAR = "_";
+	// Language Keywords
+		public static final String NIL = "nil";
+		public static final String CLASSNAME = DYNAMICSCOPECHAR + "classname";
+		public static final String CLASSES = DYNAMICSCOPECHAR + "classes";
+		public static final String SUPERCLASSES = DYNAMICSCOPECHAR + "superclasses";
+		public static final String SELF = "self";
+		public static final String _SELF = DYNAMICSCOPECHAR + SELF;
+		public static final String VALIDATE = DYNAMICSCOPECHAR + "validate";
+		public static final String LAMBDA = "lambda";
+		public static final String LAMBDAQ = "lambdaq";
+		public static final String LAMBDAM = "lambdam";
+		public static final String DICT = "dict";
+		public static final String COMMA = "comma";
+		public static final String COMMA_AT = "comma-at";
+		public static final String REST = "&rest";
+		public static final String PRINTWITHCOLON = "PRINTWITHCOLON";
+		public static final String STANDARDCLASS = "StandardClass";
+		public static final String EOF = "EOF";
+		public static final String SYMBOL = "Symbol";
+		public static final String BACKQUOTE = "backquote";
+
+		// Lexical marks 
+		public static final char COMMENTCHAR = ';';
+		public static final char BQUOTECHAR = '`';
+		public static final char QUOTECHAR = '\'';
+		public static final char COMMACHAR = ',';
+		public static final char ATCHAR = '@';
+		public static final char CDRCHAR = ':';
+
+		
+}
