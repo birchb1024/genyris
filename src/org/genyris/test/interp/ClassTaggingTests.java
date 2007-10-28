60a2893de3236356846c4ac5837c69c3ee58a787
birchb@localhost <birchb@localhost>
2007-10-28 22:37:31 +1100

    Moved example files around. Jardesc. Manual jars. Misc.

--- src/org/genyris/test/interp/ClassTaggingTests.java
+++ src/org/genyris/test/interp/ClassTaggingTests.java
@@ -4,17 +4,17 @@ import junit.framework.TestCase;
 
 public class ClassTaggingTests extends TestCase {
 
-	private TestUtilities interpreter;
+    private TestUtilities interpreter;
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		interpreter = new TestUtilities();
+    protected void setUp() throws Exception {
+        super.setUp();
+        interpreter = new TestUtilities();
         interpreter._interpreter.init(true);
-	}
+    }
 
-	private void excerciseEval(String exp, String expected) throws Exception {
-		assertEquals(expected, interpreter.eval(exp));
-	}
+    private void excerciseEval(String exp, String expected) throws Exception {
+        assertEquals(expected, interpreter.eval(exp));
+    }
 
     public void testDefineClass() throws Exception {
         excerciseEval("(class C)", "<class C (Thing) ()>");
@@ -23,16 +23,16 @@ public class ClassTaggingTests extends TestCase {
         }
 
     public void testTagWithColon() throws Exception {
-		excerciseEval("(class Miles)", "<class Miles (Thing) ()>");
-		excerciseEval("(define x 45)", "45");
+        excerciseEval("(class Miles)", "<class Miles (Thing) ()>");
+        excerciseEval("(define x 45)", "45");
         excerciseEval("(x:Miles)", "45");
         excerciseEval("(x _classes)", "(<class Miles (Thing) ()> <class Bignum (Thing) ()>)");
-       	}
+        }
 
     public void testTagWithTag() throws Exception {
         excerciseEval("(class Miles)", "<class Miles (Thing) ()>");
         excerciseEval("(define x 45)", "45");
         excerciseEval("(tag x Miles)", "45");
-        excerciseEval("(x _classes)", "(<class Miles (Thing) ()> <class Bignum (Thing) ()>)");
+        excerciseEval("(x _classes)", "(<class Bignum (Thing) ()> <class Miles (Thing) ()>)");
         }
 }
