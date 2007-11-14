80d727f6b12738ecf2d7e02d32ecb86c1c44946d
Peter william Birch <devnull@localhost>
2007-11-14 17:41:38 +1100

    Added interface checks.

--- src/org/genyris/io/StdioInStream.java
+++ src/org/genyris/io/StdioInStream.java
@@ -11,24 +11,36 @@ import java.io.IOException;
 public class StdioInStream implements InStream {
 
     private int _nextByte;
+    private boolean _gotByte;
+
+    public StdioInStream() {
+        _gotByte = false;
+    }
 
     public void unGet(char x) throws LexException {
-        ;
+        throw new LexException("StdioStream: unGet not implemented.");
     }
 
 
     public char readNext() {
+        if(!_gotByte) {
+            throw new IllegalStateException("StdioInStream: readNext() called before hasData()");
+        }
+        _gotByte = false;
         return (char)_nextByte;
     }
 
     public boolean hasData() {
         try {
             _nextByte = System.in.read();
+            _gotByte = true;
         }
         catch (IOException e) {
+            _gotByte = false;
             return false;
         }
         if( _nextByte == -1 ) {
+            _gotByte = false;
             return false;
         }
         else {
