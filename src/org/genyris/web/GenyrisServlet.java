6c5327f8f2dcf8dc243cf4de0d62be83def62591
Peter William Birch <devnull@localhost>
2008-01-14 18:15:57 +1100

    GenyrisServlet and War file now working.

--- src/org/genyris/web/GenyrisServlet.java
+++ src/org/genyris/web/GenyrisServlet.java
@@ -23,60 +23,71 @@ import org.genyris.io.Parser;
 import org.genyris.io.StringInStream;
 import org.genyris.io.UngettableInStream;
 
-
 public final class GenyrisServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
+	/**
+	 * Respond to a GET request for the content produced by this servlet.
+	 * 
+	 * @param request
+	 *            The servlet request we are processing
+	 * @param response
+	 *            The servlet response we are producing
+	 * 
+	 * @exception IOException
+	 *                if an input/output error occurs
+	 * @exception ServletException
+	 *                if a servlet error occurs
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 
-    /**
-     * Respond to a GET request for the content produced by
-     * this servlet.
-     *
-     * @param request The servlet request we are processing
-     * @param response The servlet response we are producing
-     *
-     * @exception IOException if an input/output error occurs
-     * @exception ServletException if a servlet error occurs
-     */
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-      throws IOException, ServletException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
 
-    response.setContentType("text/html");
-    PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("<title>Genyris</title>");
+		writer.println("</head>");
+		writer.println("<body bgcolor=white><PRE>");
 
-    writer.println("<html>");
-    writer.println("<head>");
-    writer.println("<title>Genyris</title>");
-    writer.println("</head>");
-    writer.println("<body bgcolor=white><PRE>");
+		Interpreter _interpreter = null;
+		Object interpattr = request.getSession().getAttribute("interpreter");
+		if (interpattr == null) {
+			try {
+				_interpreter = new Interpreter();
+				_interpreter.init(true);
+				request.getSession().setAttribute("interpreter", _interpreter);
+				writer.println("new interpreter:" + _interpreter.toString());
+			} catch (GenyrisException e) {
+				writer.println(e.getMessage());
+				writer.println("Could not construct interpreter!</PRE></body>");
+				writer.println("</html>");
+				return;
+			}
+		} else {
+			_interpreter = (Interpreter) interpattr; // TODO type check
+		}
 
-        Interpreter _interpreter;
+		try {
+			InStream input = new UngettableInStream(new ConvertEofInStream(
+					new IndentStream(new UngettableInStream(new StringInStream(
+							request.getParameter("expression"))), false)));
+			Parser parser = _interpreter.newParser(input);
+			Exp expression = parser.read();
+			Exp result = _interpreter.evalInGlobalEnvironment(expression);
 
-        try {
-            _interpreter = new Interpreter();
-            _interpreter.init(true);
+			Formatter formatter = new IndentedFormatter(writer, 3, _interpreter);
+			result.acceptVisitor(formatter);
+		} catch (GenyrisException e) {
+			writer.println(e.getMessage());
+			writer.println(e.getStackTrace().toString());
+		}
 
-            InStream input = new UngettableInStream(new ConvertEofInStream(
-                    new IndentStream(
-                            new UngettableInStream(new StringInStream("list \"Hello\" \"world\"")), false)));
-            Parser parser = _interpreter.newParser(input);
-            Exp expression = parser.read();
-            Exp result = _interpreter.evalInGlobalEnvironment(expression);
+		writer.println("</PRE></body>");
+		writer.println("</html>");
 
-            Formatter formatter = new IndentedFormatter(writer, 3, _interpreter);
-            result.acceptVisitor(formatter);
-        } catch (GenyrisException e) {
-            writer.print(e.getStackTrace());
-        }
-
-
-    
-    writer.println("</PRE></body>");
-    writer.println("</html>");
-
-    }
-
+	}
 
 }
