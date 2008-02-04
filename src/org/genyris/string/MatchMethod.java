b06684296702f2897c56f80001a7a4ae281d0426
Peter William Birch <devnull@localhost>
2008-02-04 19:58:53 +1100

    Added String _match method.

---
+++ src/org/genyris/string/MatchMethod.java
@@ -0,0 +1,36 @@
+// Copyright 2008 Peter William Birch <birchb@genyis.org>
+//
+// This software may be used and distributed according to the terms
+// of the Genyris License, in the file "LICENSE", incorporated herein by reference.
+//
+package org.genyris.string;
+
+import org.genyris.core.Constants;
+import org.genyris.core.Exp;
+import org.genyris.core.Lstring;
+import org.genyris.exception.GenyrisException;
+import org.genyris.interp.Closure;
+import org.genyris.interp.Environment;
+import org.genyris.interp.Interpreter;
+
+public class MatchMethod extends AbstractStringMethod {
+
+	public MatchMethod(Interpreter interp) {
+		super(interp);
+	}
+
+	public Exp bindAndExecute(Closure proc, Exp[] arguments, Environment env)
+			throws GenyrisException {
+		if(arguments.length > 0) {
+			if(!(arguments[0] instanceof Lstring)) {
+				throw new GenyrisException("Non string passed to " + Constants.MATCH);
+			}
+			Lstring regex = (Lstring) arguments[0];			
+			Lstring theString = getSelfString(env);
+			return theString.match(NIL, TRUE, regex);
+		}
+		else {
+			throw new GenyrisException("Missing argument to " + Constants.MATCH);			
+		}
+	}
+}
