bc0b8af6c4273c80d9671d75b2cf2311197b9e12
Peter William Birch <devnull@localhost>
2008-02-17 22:12:27 +1100

    Added request class to httpd, and Alist class.

--- src/org/genyris/test/string/StringTests.java
+++ src/org/genyris/test/string/StringTests.java
@@ -8,55 +8,51 @@ import junit.framework.TestCase;
 
 public class StringTests extends TestCase {
 
-	private TestUtilities interpreter;
+    private TestUtilities interpreter;
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		interpreter = new TestUtilities();
-	}
+    protected void setUp() throws Exception {
+        super.setUp();
+        interpreter = new TestUtilities();
+    }
 
-	private void checkEval(String exp, String expected) throws GenyrisException {
-		assertEquals(expected, interpreter.eval(exp));
-	}
+    private void checkEval(String exp, String expected) throws GenyrisException {
+        assertEquals(expected, interpreter.eval(exp));
+    }
 
-	private void checkEvalBad(String exp) throws GenyrisException {
-		try {
-			interpreter.eval(exp);
-			fail("expecting exception");
-		} catch (GenyrisException e) {
-		}
-	}
+    private void checkEvalBad(String exp) throws GenyrisException {
+        try {
+            interpreter.eval(exp);
+            fail("expecting exception");
+        } catch (GenyrisException e) {
+        }
+    }
 
-	private void eval(String script) throws GenyrisException {
-		interpreter.eval(script);
-	}
+    public void testStringSplit() throws GenyrisException {
+        checkEval("(\"\"(_split))", "(\"\")");
+        checkEval("(\"1 2 3 4 5\"(_split))", "(\"1\" \"2\" \"3\" \"4\" \"5\")");
+        checkEval("(\"1,2,3,4,5\"(_split \",\"))", "(\"1\" \"2\" \"3\" \"4\" \"5\")");
+        checkEval("(\"1    2 \t3  4 5\"(_split \"[ \\t]+\"))", "(\"1\" \"2\" \"3\" \"4\" \"5\")");
 
-	public void testStringSplit() throws GenyrisException {
-		checkEval("(\"\"(_split))", "(\"\")");
-		checkEval("(\"1 2 3 4 5\"(_split))", "(\"1\" \"2\" \"3\" \"4\" \"5\")");
-		checkEval("(\"1,2,3,4,5\"(_split \",\"))", "(\"1\" \"2\" \"3\" \"4\" \"5\")");
-		checkEval("(\"1    2 \t3  4 5\"(_split \"[ \\t]+\"))", "(\"1\" \"2\" \"3\" \"4\" \"5\")");
+        checkEvalBad("((String_split) \"A\" \"B\")");
 
-		checkEvalBad("((String_split) \"A\" \"B\")");
+        checkEval("(\"http://www.genyris.org/path/index.html\"(_split \"http://\"))", "(\"\" \"www.genyris.org/path/index.html\")");
 
-		checkEval("(\"http://www.genyris.org/path/index.html\"(_split \"http://\"))", "(\"\" \"www.genyris.org/path/index.html\")");
+    }
 
-	}
-
-	public void testStringConcat() throws GenyrisException {
-		checkEval("(\"\"(_+))", "\"\"");
-		checkEval("(\"A\"(_+))", "\"A\"");
-		checkEval("(\"A\"(_+ \"B\"))", "\"AB\"");
-		checkEval("(\"A\"(_+ \"B\" \"C\"))", "\"ABC\"");
-	}
-	public void testStringMatch() throws GenyrisException {
-		checkEval("(\"abc\"(_match \"a.c\"))", "true");
-		checkEval("(\"abc\"(_match \"a.d\"))", "nil");
-		checkEval("(\"\"(_match \"\"))", "true");
-		checkEvalBad("(3(_match \"\"))");
-		checkEvalBad("(\"A\"(_match))");
-		checkEvalBad("(\"A\"(_match 34))");
-		checkEval("(\"http://www.genyris.org/path/index.html\"(_match \"http://[^/]+/.*\"))", "true");
-	}
+    public void testStringConcat() throws GenyrisException {
+        checkEval("(\"\"(_+))", "\"\"");
+        checkEval("(\"A\"(_+))", "\"A\"");
+        checkEval("(\"A\"(_+ \"B\"))", "\"AB\"");
+        checkEval("(\"A\"(_+ \"B\" \"C\"))", "\"ABC\"");
+    }
+    public void testStringMatch() throws GenyrisException {
+        checkEval("(\"abc\"(_match \"a.c\"))", "true");
+        checkEval("(\"abc\"(_match \"a.d\"))", "nil");
+        checkEval("(\"\"(_match \"\"))", "true");
+        checkEvalBad("(3(_match \"\"))");
+        checkEvalBad("(\"A\"(_match))");
+        checkEvalBad("(\"A\"(_match 34))");
+        checkEval("(\"http://www.genyris.org/path/index.html\"(_match \"http://[^/]+/.*\"))", "true");
+    }
 
 }
