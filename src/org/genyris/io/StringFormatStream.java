fbda4376f7e14bcd35bdf2a3c7d24fd555d73b9b
Peter William Birch <devnull@localhost>
2008-02-24 23:58:51 +1100

    Added StringFormat stream.

---
+++ src/org/genyris/io/StringFormatStream.java
@@ -0,0 +1,112 @@
+// Copyright 2008 Peter William Birch <birchb@genyis.org>
+//
+// This software may be used and distributed according to the terms
+// of the Genyris License, in the file "LICENSE", incorporated herein by reference.
+//
+package org.genyris.io;
+
+public class StringFormatStream implements InStreamEOF {
+
+    private static final int IN_A_STRING = 0;
+
+    private static final int STARTING = 1;
+
+    private static final int ENDING = 2;
+
+    private static final int ESCAPE = 3;
+
+    private static final int IN_LISP = 4;
+
+    InStream _instream;
+
+    int _parseState;
+
+    char _escaped;
+
+    public StringFormatStream(InStream in) {
+        _instream = in;
+        _parseState = STARTING;
+    }
+
+    public int getChar() throws LexException {
+
+        char ch = '@';
+
+        switch (_parseState) {
+
+        case IN_A_STRING:
+            if (!_instream.hasData()) {
+                _parseState = ENDING;
+                return '"';
+            }
+            ch = _instream.readNext();
+            if (ch == '\\') {
+                _parseState = ESCAPE;
+                _escaped = '\\';
+                return '\\';
+            }
+            else if (ch == '"') {
+                _parseState = ESCAPE;
+                _escaped = '"';
+                return '\\';
+            }
+            else if (ch == '<') {
+                if (!_instream.hasData()) {
+                    _parseState = ENDING;
+                    return ch;
+                }
+                char ch2 = _instream.readNext();
+                if (ch2 == '%') {
+                    _parseState = IN_LISP;
+                    return '"'; // end current string
+                }
+                else {
+                    _instream.unGet(ch2); // parse this char again
+                    return '<';
+                }
+            }
+            else {
+                return ch;
+            }
+
+        case IN_LISP:
+            if (!_instream.hasData()) {
+                _parseState = ENDING;
+                return ch;
+            }
+            ch = _instream.readNext();
+            if (ch == '%') {
+                if (!_instream.hasData()) {
+                    _parseState = ENDING;
+                    return ch;
+                }
+                char ch2 = _instream.readNext();
+                if (ch2 == '>') {
+                    _parseState = IN_A_STRING;
+                    return '"'; // start new string
+                }
+                else {
+                    _instream.unGet(ch2); // parse this char again
+                    return '%';
+                }
+            }
+            else {
+                return ch;
+            }
+
+        case STARTING:
+            _parseState = IN_A_STRING;
+            ch = '"';
+            return ch;
+
+        case ENDING: // End
+            return EOF;
+
+        case ESCAPE: // escape
+            _parseState = IN_A_STRING;
+            return _escaped;
+
+        }
+        return EOF;
+    }
+}
---
+++ src/org/genyris/test/io/StringFormatStreamTest.java
@@ -0,0 +1,62 @@
+// Copyright 2008 Peter William Birch <birchb@genyis.org>
+//
+// This software may be used and distributed according to the terms
+// of the Genyris License, in the file "LICENSE", incorporated herein by reference.
+//
+package org.genyris.test.io;
+
+import junit.framework.TestCase;
+
+import org.genyris.io.ConvertEofInStream;
+import org.genyris.io.InStream;
+import org.genyris.io.LexException;
+import org.genyris.io.StringFormatStream;
+import org.genyris.io.StringInStream;
+import org.genyris.io.UngettableInStream;
+
+public class StringFormatStreamTest extends TestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    private void excerciseSFS(String toparse, String expected) throws LexException {
+        InStream ind = new ConvertEofInStream(new StringFormatStream(new UngettableInStream(
+                new StringInStream(toparse))));
+        String result = "";
+        while (ind.hasData()) {
+            result += ind.readNext();
+        }
+        assertEquals(expected, result);
+    }
+
+
+
+    public void testSFStreamEmpty() throws LexException {
+        excerciseSFS("", "\"\"");
+    }
+    public void testSFStream2() throws LexException {
+        excerciseSFS("1", "\"1\"");
+    }
+    public void testSFStream3() throws LexException {
+        excerciseSFS("1", "\"1\"");
+    }
+    public void testSFStream4() throws LexException {
+        excerciseSFS("12343567890", "\"12343567890\"");
+    }
+    public void testSFStream5() throws LexException {
+        excerciseSFS("<%%>", "\"\"\"\"");
+    }
+    public void testSFStream6() throws LexException {
+        excerciseSFS("<%alpha%>", "\"\"alpha\"\"");
+    }
+    public void testSFStream7() throws LexException {
+        excerciseSFS("one<%alpha%>two", "\"one\"alpha\"two\"");
+    }
+
+    public void testSFStream8() throws LexException {
+        excerciseSFS("on\"e<%alpha%>two", "\"on\\\"e\"alpha\"two\"");
+    }
+
+
+}
