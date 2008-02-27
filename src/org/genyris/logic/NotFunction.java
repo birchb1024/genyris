55fcd01656df5f90927d2ff8967a1be7819171df
Peter William Birch <devnull@localhost>
2008-02-27 19:55:06 +1100

    Added not.

---
+++ src/org/genyris/logic/NotFunction.java
@@ -0,0 +1,30 @@
+// Copyright 2008 Peter William Birch <birchb@genyis.org>
+//
+// This software may be used and distributed according to the terms
+// of the Genyris License, in the file "LICENSE", incorporated herein by reference.
+//
+package org.genyris.logic;
+
+import org.genyris.core.Exp;
+import org.genyris.core.Lsymbol;
+import org.genyris.exception.GenyrisException;
+import org.genyris.interp.ApplicableFunction;
+import org.genyris.interp.Closure;
+import org.genyris.interp.Environment;
+import org.genyris.interp.Interpreter;
+
+public class NotFunction extends ApplicableFunction {
+
+    public NotFunction(Interpreter interp, Lsymbol name) {
+        super(interp, name);
+    }
+
+    public Exp bindAndExecute(Closure proc, Exp[] arguments, Environment envForBindOperations)
+            throws GenyrisException {
+        if (arguments.length < 1)
+            throw new GenyrisException("Too few arguments to not: " + arguments.length);
+        return (arguments[0] == NIL) ? TRUE : NIL;
+
+    }
+
+}
