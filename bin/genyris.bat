4e6ee40d6fcd03f5f2fd9b6c2808012ae872d874
birchb <devnull@localhost>
2009-02-25 20:02:53 +1100

    Updates to reflect new binary release in a zip file.

--- bin/genyris.bat
+++ bin/genyris.bat
@@ -1,2 +1,2 @@
 ::
-java -jar %GENYRIS_HOME%\dist\genyris-bin*.jar $0 $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -cp %GENYRIS_HOME%\dist\genyris-bin*.jar org.genyris.interp.ClassicReadEvalPrintLoop %1 %2 %3 %4 %5 %6 %7 %8 %9
