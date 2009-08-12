c16196f2e259418720d8274443f65e5fd42aea44
birchb <devnull@localhost>
2009-08-12 19:23:03 +1000

    Updated examples with use of client IP in HTTP Request.

--- examples/notification-server.bat
+++ examples/notification-server.bat
@@ -1,2 +1,2 @@
 :: start Sub Lamp server
-start "Notification Server" /MIN java -jar -cp . genyris-bin*.jar -eval do (web.serve 8080 \"C:/workspace/notifications/notification-server.lin\") (read)
+start "Notification Server" /MIN java -jar -cp . genyris-bin*.jar -eval do (web.serve 8111 \"C:/workspace/genyris/examples/notification-server.lin\") (read)
