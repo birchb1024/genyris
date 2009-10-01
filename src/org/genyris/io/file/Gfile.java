b6f8aa3fc97959d8972770d9b430e8475e3ba922
birchb <devnull@localhost>
2009-10-01 18:48:58 +1000

    Added functions to File static-list-dir statis-is-dir? static-abs-path

--- src/org/genyris/io/file/Gfile.java
+++ src/org/genyris/io/file/Gfile.java
@@ -5,13 +5,16 @@
 //
 package org.genyris.io.file;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import org.genyris.core.Exp;
+import org.genyris.core.Pair;
 import org.genyris.core.StrinG;
 import org.genyris.core.Symbol;
 import org.genyris.exception.GenyrisException;
@@ -56,7 +59,74 @@ public class Gfile {
             return open((StrinG)arguments[0], (Symbol)arguments[1]);
         }
     }
+    public static class FileListDir extends AbstractMethod {
+    	private static Class[] types = {StrinG.class};
+
+        public FileListDir(Interpreter interp) {
+            super(interp, "static-list-dir");
+        }
+
+        public Exp bindAndExecute(Closure proc, Exp[] arguments, Environment env)
+                throws GenyrisException {
+        	Exp retval = NIL;
+        	checkMinArguments(arguments, 1);
+        	checkArgumentTypes(types, arguments);
+        		File dir = new File(arguments[0].toString());
+        		String[] children = dir.list();
+        		
+        		if (children == null) {
+        	        throw new GenyrisException("File.static-list-dir: failed on " + dir);
+        	        // Either dir does not exist or is not a directory
+        	    } else {
+        	        for (int i=children.length-1; i>=0; i--) {
+        	            retval = new Pair( new StrinG(children[i]), retval);
+        	        }
+        	    }
+            return retval;
+        }
+    }
+
+    public static class IsDir extends AbstractMethod {
+    	private static Class[] types = {StrinG.class};
+
+        public IsDir(Interpreter interp) {
+            super(interp, "static-is-dir?");
+        }
+
+        public Exp bindAndExecute(Closure proc, Exp[] arguments, Environment env)
+                throws GenyrisException {
+        	checkMinArguments(arguments, 1);
+        	checkArgumentTypes(types, arguments);
+        	File dir = new File(arguments[0].toString());
+        	boolean isDir = dir.isDirectory();
+
+            return (isDir? TRUE: NIL);
+        }
+    }
+    public static class FileAbsPath extends AbstractMethod {
+    	private static Class[] types = {StrinG.class};
+
+        public FileAbsPath(Interpreter interp) {
+            super(interp, "static-abs-path");
+        }
+
+        public Exp bindAndExecute(Closure proc, Exp[] arguments, Environment env)
+                throws GenyrisException {
+        	checkMinArguments(arguments, 1);
+        	checkArgumentTypes(types, arguments);
+        	File file = new File(arguments[0].toString());
+        	
+            try {
+				return new StrinG(file.getCanonicalPath());
+			} catch (IOException e) {
+				throw new GenyrisException("static-abs-path: " + e.getMessage());
+			}
+        }
+    }
     public static void bindFunctionsAndMethods(Interpreter interpreter) throws UnboundException, GenyrisException {
         interpreter.bindMethodInstance("File", new Gfile.FileOpenMethod(interpreter));
+        interpreter.bindMethodInstance("File", new Gfile.FileListDir(interpreter));
+        interpreter.bindMethodInstance("File", new Gfile.FileAbsPath(interpreter));
+        interpreter.bindMethodInstance("File", new Gfile.IsDir(interpreter));
     }
 }
