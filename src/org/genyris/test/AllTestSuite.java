b1ff1964d5df9af4694100c80f9be34eda8df1f7
birchb@asusu50v <birchb@asusu50v>
2010-03-09 00:15:32 +1100

    Added missing junits to test-runner.

--- src/org/genyris/test/AllTestSuite.java
+++ src/org/genyris/test/AllTestSuite.java
@@ -4,6 +4,8 @@ package org.genyris.test;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.genyris.test.core.*;
+import org.genyris.test.dl.TripleStoreTest;
+import org.genyris.test.dl.TripleTest;
 import org.genyris.test.format.*;
 import org.genyris.test.interp.BuiltinInterpreterTests;
 import org.genyris.test.interp.ClassTaggingTests;
@@ -14,8 +16,10 @@ import org.genyris.test.interp.InterpretedTests;
 import org.genyris.test.interp.ObjectOrientationTests;
 import org.genyris.test.io.IndentStreamTest;
 import org.genyris.test.io.SourceLoaderTest;
+import org.genyris.test.io.StringFormatStreamTest;
 import org.genyris.test.io.StringInStreamTest;
 import org.genyris.test.io.UngettableInStreamTest;
+import org.genyris.test.string.StringTests;
 
 public class AllTestSuite {
 
@@ -23,17 +27,26 @@ public class AllTestSuite {
 
         TestSuite suite = new TestSuite("AllTestSuite");
 
+        // core
         suite.addTestSuite(CoreTest.class);
         suite.addTestSuite(DictTest.class);
         suite.addTestSuite(EqualityTest.class);
         suite.addTestSuite(LexTest.class);
         suite.addTestSuite(LexTestNumbers.class);
         suite.addTestSuite(LsymbolTest.class);
+        suite.addTestSuite(SymbolTableTest.class);
+        // dl
+        suite.addTestSuite(TripleStoreTest.class);
+        suite.addTestSuite(TripleTest.class);
+        // string
+        suite.addTestSuite(StringTests.class);
 
+        // format
         suite.addTestSuite(BasicFormatterTest.class);
         suite.addTestSuite(IndentedFormatterTest.class);
         suite.addTestSuite(RoundtripIndentedFormatterTest.class);
 
+        // interp
         suite.addTestSuite(BuiltinInterpreterTests.class);
         suite.addTestSuite(ClassTaggingTests.class);
         suite.addTestSuite(ComplexInterpreterTests.class);
@@ -42,8 +55,10 @@ public class AllTestSuite {
         suite.addTestSuite(InterpretedTests.class);
         suite.addTestSuite(ObjectOrientationTests.class);
 
+        // IO
         suite.addTestSuite(IndentStreamTest.class);
         suite.addTestSuite(SourceLoaderTest.class);
+        suite.addTestSuite(StringFormatStreamTest.class);
         suite.addTestSuite(StringInStreamTest.class);
         suite.addTestSuite(UngettableInStreamTest.class);
 
