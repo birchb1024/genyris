a7ed3b6fe08ff59a982554bf0d3d342ced5513bf
birchb <devnull@localhost>
2010-04-09 18:31:28 +1000

    (no commit message)

---
+++ src/org/genyris/java/swing/GPanel.java
@@ -0,0 +1,21 @@
+package org.genyris.java.swing;
+
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JPanel;
+public class GPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private GenyrisActionListener repaint;
+
+	public GPanel(GenyrisActionListener closure) {
+		super();
+		repaint = closure;
+	}
+
+	public void paintComponent(Graphics g) {
+		if(repaint != null)
+			repaint.actionPerformed(new ActionEvent(g, 0, "repaint"));
+	}
+}
