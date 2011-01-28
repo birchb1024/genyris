b36ea6c2f81e95e7139a927a6ee2468d2b0eb87b
birchb <devnull@localhost>
2011-01-28 14:39:16 +1100

    Added String methods .slice .toInts .fromInts (take 2)

---
+++ src/org/genyris/string/FromIntsMethod.java
@@ -0,0 +1,29 @@
+// Copyright 2008 Peter William Birch <birchb@genyis.org>
+//
+// This software may be used and distributed according to the terms
+// of the Genyris License, in the file "LICENSE", incorporated herein by reference.
+//
+package org.genyris.string;
+
+import org.genyris.core.Constants;
+import org.genyris.core.Exp;
+import org.genyris.core.StrinG;
+import org.genyris.exception.GenyrisException;
+import org.genyris.interp.Closure;
+import org.genyris.interp.Environment;
+import org.genyris.interp.Interpreter;
+
+public class FromIntsMethod extends AbstractStringMethod {
+
+	public static String getStaticName() {return Constants.FROMINTS;};
+	
+	public FromIntsMethod(Interpreter interp) {
+        super(interp, getStaticName());
+    }
+
+    public Exp bindAndExecute(Closure proc, Exp[] arguments, Environment env)
+			throws GenyrisException {
+    	checkArguments(arguments, 1);
+		return StrinG.makeStringFromInts(NIL, arguments[0]);
+	}
+}
---
+++ src/org/genyris/string/ToIntsMethod.java
@@ -0,0 +1,34 @@
+// Copyright 2008 Peter William Birch <birchb@genyis.org>
+//
+// This software may be used and distributed according to the terms
+// of the Genyris License, in the file "LICENSE", incorporated herein by reference.
+//
+package org.genyris.string;
+
+import org.genyris.core.Constants;
+import org.genyris.core.Exp;
+import org.genyris.core.StrinG;
+import org.genyris.exception.GenyrisException;
+import org.genyris.interp.Closure;
+import org.genyris.interp.Environment;
+import org.genyris.interp.Interpreter;
+
+public class ToIntsMethod extends AbstractStringMethod {
+
+	public static String getStaticName() {return Constants.TOINTS;};
+	
+	public ToIntsMethod(Interpreter interp) {
+        super(interp, getStaticName());
+    }
+
+    public Exp bindAndExecute(Closure proc, Exp[] arguments, Environment env)
+			throws GenyrisException {
+		if(arguments.length > 0) {
+			if(!(arguments[0] instanceof StrinG)) {
+				throw new GenyrisException("Non string passed to " + Constants.SPLIT);
+			}
+		}
+		StrinG theString = getSelfString(env);
+		return theString.toInts(NIL);
+	}
+}
