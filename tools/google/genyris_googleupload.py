f4f54ff8d05c613403ce957b71be70069d6765d6
Peter William Birch <birchb1024@gmail.com>
2013-04-13 18:53:00 +1000

    Googlecode uploadscript tested.

--- tools/google/genyris_googleupload.py
+++ tools/google/genyris_googleupload.py
@@ -2,26 +2,29 @@ import googlecode_upload
 import sys, os
 
 if len(sys.argv) != 2:
-   raise "missing version number."
+   raise Exception("missing version number.")
 version = sys.argv[1]   
-winversion = version.replace('.','_')  
+underversion = version.replace('.','_')  
 print "Uploading version %s" % version
 home = os.getenv('GENYRIS_HOME')
 password = os.getenv('GOOGLE_PASSWORD')
 
 def gupload(file, summary, labels):
-   print googlecode_upload.upload("%s/rel/%s" % (home,file), 
+   statuscode, msg, url = googlecode_upload.upload("%s/rel/%s" % (home,file), 
       'Genyris', 
       'birchb1024@gmail.com',
       password,
       summary % version, labels)
-   
-gupload("genyris-binary-%s.zip.MD5" % version, "Genyris %s binary checksum", ['Type-Docs'])
-gupload("genyris-binary-%s.zip" % version, "Genyris %s binary", ['OpSys-All'])
-gupload("genyris_linux_%s.deb" % version, "Genyris %s Debian package", ['Type-Installer','OpSys-Linux'])
+   print "upload: %s %s %s" % (statuscode, msg, url)
+   if( statuscode != 201):
+	 raise Exception("Googlecode upload failed: %s %s %s" % (statuscode, msg, url))
+#gupload("genyris-binary-%s.zip.MD5" % version, "Genyris %s binary checksum", ['Type-Docs'])
+#gupload("genyris_linux_%s.deb.MD5" % version, "Genyris %s Debian package checksum", ['Type-Docs'])
+#gupload("genyris-binary-%s.zip" % version, "Genyris %s binary", ['OpSys-All'])
+gupload("genyris_linux_%s.deb" % underversion, "Genyris %s Debian package", ['Type-Installer','OpSys-Linux'])
 gupload("genyris-manual-%s.html" % version, "Genyris %s Manual (HTML)", ['Type-Docs'])
 gupload("genyris-manual-%s.pdf" % version, "Genyris %s Manual (PDF)", ['Type-Docs'])
-gupload("genyris_windows_%s.exe" % winversion, "Genyris %s Windows Installer", ['Type-Installer','OpSys-Windows'])
+gupload("genyris_windows_%s.exe" % underversion, "Genyris %s Windows Installer", ['Type-Installer','OpSys-Windows'])
 
 
 
