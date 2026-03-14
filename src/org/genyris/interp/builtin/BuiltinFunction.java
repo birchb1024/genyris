70371fe983b1918ccc8a2f4498da43687bd5b205
Bill Birch <bbirch@stan.com.au>
2026-03-14 15:11:04 +1100

    Add slash builtin function

--- src/org/genyris/interp/builtin/BuiltinFunction.java
+++ src/org/genyris/interp/builtin/BuiltinFunction.java
@@ -56,6 +56,7 @@ public abstract class BuiltinFunction  extends ApplicableFunction {
         interpreter.bindGlobalProcedureInstance(new ReplaceCdrFunction(interpreter));
         interpreter.bindGlobalProcedureInstance(new ReverseFunction(interpreter));
         interpreter.bindGlobalProcedureInstance(new SetFunction(interpreter));
+        interpreter.bindGlobalProcedureInstance(new SlashFunction(interpreter));
         interpreter.bindGlobalProcedureInstance(new SortFunction(interpreter));
         interpreter.bindGlobalProcedureInstance(new SymbolValueFunction(interpreter));
         interpreter.bindGlobalProcedureInstance(new SymbolPrefixFunction(interpreter));
